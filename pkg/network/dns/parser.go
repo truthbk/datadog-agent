aac42cc39a424147e7bc6994edec618f2f8c869e
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2022-09-05 11:24:44 +0200

    s|linux_bpf|linux|g

--- pkg/network/dns/parser.go
+++ pkg/network/dns/parser.go
@@ -3,8 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build windows || linux_bpf
-// +build windows linux_bpf
+//go:build windows || linux
+// +build windows linux
 
 package dns
 
