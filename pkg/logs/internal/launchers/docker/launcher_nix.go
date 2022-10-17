c9c64c6407a210e8ff9832c4d43c33964a603cf7
Lucas Liseth <lucas.liseth@datadoghq.com>
2022-10-17 13:42:11 -0400

    Added print logging for testing

--- pkg/logs/internal/launchers/docker/launcher_nix.go
+++ pkg/logs/internal/launchers/docker/launcher_nix.go
@@ -30,6 +30,7 @@ func getDockerLogsPath() string {
 	if usePodmanLogs {
 		return podmanBasePath
 	} else if len(overridePath) > 0 {
+		fmt.Println("THE PATH IS: ", overridePath)
 		return overridePath
 	}
 
