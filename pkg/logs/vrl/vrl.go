322b4dea02a8a3864f06e805ab8787ba4b44b98f
Brian Floersch <brian.floersch@datadoghq.com>
2022-11-16 09:38:23 -0500

    linker flags

--- pkg/logs/vrl/vrl.go
+++ pkg/logs/vrl/vrl.go
@@ -5,7 +5,7 @@ package vrl
 //#include <string.h>
 //#include <vrl.h>
 //#cgo CFLAGS: -I${SRCDIR}/pkg/logs/vrl/target/release/
-//#cgo LDFLAGS: -L${SRCDIR}/target/release -Wl,-rpath,${SRCDIR}/target/release -lvrl_bridge -lm
+//#cgo LDFLAGS: -L${SRCDIR}/target/release -Wl,-rpath,${SRCDIR}/target/release -lvrl_bridge -lm -ldl
 import "C"
 import "unsafe"
 
