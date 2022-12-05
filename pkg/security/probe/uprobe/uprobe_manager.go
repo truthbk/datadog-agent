a1a328e634673ccb48f95e07ec591f5b7dd677b8
YoannGh <yoann.ghigoff@datadoghq.com>
2022-12-05 14:38:29 +0100

    inherit rules arguments

--- pkg/security/probe/uprobe/uprobe_manager.go
+++ pkg/security/probe/uprobe/uprobe_manager.go
@@ -45,10 +45,9 @@ var uman *uprobeManager
 type uprobeManager struct {
 	lock             sync.Mutex
 	options          UProbeManagerOptions
-	allUProbes       map[uint64]*uprobe               // id to uprobe map
-	ruleFilesUprobes map[string][]*uprobe             // path to list of uprobe map
-	containerUProbes map[uint32][]*uprobe             // container pid one to uprobe map
-	ruleVulnArgs     map[uint64]*model.UProbeVulnArgs // id to uprobe args
+	allUProbes       map[uint64]*uprobe   // id to uprobe map
+	ruleFilesUprobes map[string][]*uprobe // path to list of uprobe map
+	containerUProbes map[uint32][]*uprobe // container pid one to uprobe map
 	m                *manager.Manager
 	uprobeFreeList   chan *uprobe
 	nextRuleID       uint64
@@ -69,32 +68,25 @@ type uprobeArg struct {
 
 type uprobe struct {
 	desc   uprobeDesc
-	args   [5]uprobeArg
+	args   []uprobeArg
 	id     uint64
 	ruleID uint64
 	pID    manager.ProbeIdentificationPair
 }
 
-func Init(manager *manager.Manager, options UProbeManagerOptions) error {
-	if uman == nil {
-		uman = &uprobeManager{
-			allUProbes:       make(map[uint64]*uprobe),
-			ruleFilesUprobes: make(map[string][]*uprobe),
-			containerUProbes: make(map[uint32][]*uprobe),
-			ruleVulnArgs:     make(map[uint64]*model.UProbeVulnArgs),
-			m:                manager,
-			options:          options,
-		}
-
-		if options.MaxConcurrentUProbes == 0 {
-			uman.options.MaxConcurrentUProbes = DefaultMaxConcurrentUProbes
-		}
-		uman.uprobeFreeList = make(chan *uprobe, uman.options.MaxConcurrentUProbes)
-		for i := uint64(0); i < uman.options.MaxConcurrentUProbes; i++ {
-			putUProbe(&uprobe{id: i})
-		}
+func newUProbe(id uint64) *uprobe {
+	return &uprobe{
+		id:   id,
+		args: make([]uprobeArg, 5),
 	}
-	return nil
+}
+
+func newUProbeArgs() *model.UProbeVulnArgs {
+	args := &model.UProbeVulnArgs{}
+	for i := 0; i < 5; i++ {
+		args.Args[i].Tocheck = false
+	}
+	return args
 }
 
 func getNextRuleID() uint64 {
@@ -116,6 +108,43 @@ func putUProbe(up *uprobe) {
 	if up != nil {
 		uman.uprobeFreeList <- up
 	}
+}
+
+func pushRuleArgs(ruleID uint64, vargs *model.UProbeVulnArgs) error {
+	vulnargsMap, found, err := uman.m.GetMap("vulnargs")
+	if err != nil {
+		return err
+	}
+	if !found {
+		return fmt.Errorf("couldn't find vulnargs map")
+	}
+
+	err = vulnargsMap.Put(ruleID, vargs)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Init(manager *manager.Manager, options UProbeManagerOptions) error {
+	if uman == nil {
+		uman = &uprobeManager{
+			allUProbes:       make(map[uint64]*uprobe),
+			ruleFilesUprobes: make(map[string][]*uprobe),
+			containerUProbes: make(map[uint32][]*uprobe),
+			m:                manager,
+			options:          options,
+		}
+
+		if options.MaxConcurrentUProbes == 0 {
+			uman.options.MaxConcurrentUProbes = DefaultMaxConcurrentUProbes
+		}
+		uman.uprobeFreeList = make(chan *uprobe, uman.options.MaxConcurrentUProbes)
+		for i := uint64(0); i < uman.options.MaxConcurrentUProbes; i++ {
+			putUProbe(newUProbe(i))
+		}
+	}
+	return nil
 }
 
 func ResolveUProbeEventFields(event *model.UProbeEvent) {
@@ -216,6 +245,7 @@ func CreateUProbeFromRules(rules []*rules.Rule) error {
 			var err error
 			up.args[i].parsed, err = argParser.ParseString("", argValue)
 			if err != nil || up.args[i].parsed == nil {
+				putUProbe(up)
 				return ErrUProbeRuleInvalidArgumentExpression
 			}
 			up.args[i].txt = argValue
@@ -342,6 +372,7 @@ func HandleNewMountNamespace(event *model.NewMountNSEvent) error {
 			newUProbe.desc.OffsetStr = up.desc.OffsetStr
 			newUProbe.desc.Offset = up.desc.Offset
 			newUProbe.ruleID = up.ruleID
+			newUProbe.args = up.args[:]
 
 			err := attachProbe(uman.m, newUProbe)
 			if err != nil {
@@ -350,7 +381,7 @@ func HandleNewMountNamespace(event *model.NewMountNSEvent) error {
 				continue
 			}
 
-			uman.allUProbes[up.id] = newUProbe
+			uman.allUProbes[newUProbe.id] = newUProbe
 			uman.containerUProbes[event.PidOne] = append(uman.containerUProbes[event.PidOne], newUProbe)
 
 			seclog.Infof("attached uprobe %s %s:%s", newUProbe.pID.UID, newUProbe.desc.Path, newUProbe.desc.FunctionName)
@@ -374,28 +405,4 @@ func HandleProcessExit(event *model.ExitEvent) {
 		putUProbe(up)
 	}
 	delete(uman.containerUProbes, event.PIDContext.Tid)
-}
-
-func NewUProbeArgs() *model.UProbeVulnArgs {
-	args := &model.UProbeVulnArgs{}
-	for i := 0; i < 5; i++ {
-		args.Args[i].Tocheck = false
-	}
-	return args
-}
-
-func pushRuleArgs(ruleID uint64, vargs *model.UProbeVulnArgs) error {
-	vulnargsMap, found, err := uman.m.GetMap("vulnargs")
-	if err != nil {
-		return err
-	}
-	if !found {
-		return fmt.Errorf("couldn't find vulnargs map")
-	}
-
-	err = vulnargsMap.Put(ruleID, vargs)
-	if err != nil {
-		return err
-	}
-	return nil
 }
