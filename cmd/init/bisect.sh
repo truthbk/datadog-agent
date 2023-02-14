f0c93174715423a97fc9b8d5d934c7f73519243e
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-02-14 09:36:38 -0800

    Add more config

--- cmd/init/bisect.sh
+++ cmd/init/bisect.sh
@@ -3,51 +3,53 @@
 set -e
 set -o pipefail
 
+#./scripts/config --disable CONFIG_ACPI_AC && \
+#./scripts/config --disable CONFIG_ACPI_BATTERY && \
+#./scripts/config --disable CONFIG_ACPI_BUTTON && \
+#./scripts/config --disable CONFIG_ACPI_DOCK && \
+#./scripts/config --disable CONFIG_ACPI_FAN && \
+#./scripts/config --disable CONFIG_ACPI_THERMAL && \
+#./scripts/config --disable CONFIG_ACPI_TINY_POWER_BUTTON && \
+#./scripts/config --disable CONFIG_ACPI_VIDEO && \
+#./scripts/config --enable CONFIG_BRIDGE && \
+#./scripts/config --disable CONFIG_DRM && \
+#./scripts/config --disable CONFIG_EFI && \
+#./scripts/config --disable CONFIG_ETHTOOL_NETLINK && \
+#./scripts/config --disable CONFIG_EXT4_FS && \
+#./scripts/config --disable CONFIG_HID && \
+#./scripts/config --disable CONFIG_INPUT && \
+#./scripts/config --disable CONFIG_INPUT_MOUSE && \
+#./scripts/config --disable CONFIG_IPV6 && \
+#./scripts/config --disable CONFIG_MACINTOSH_DRIVERS && \
+#./scripts/config --enable CONFIG_NETFILTER && \
+#./scripts/config --disable CONFIG_NETWORK_FILESYSTEMS && \
+#./scripts/config --disable CONFIG_PCMCIA && \
+#./scripts/config --disable CONFIG_PM && \
+#./scripts/config --disable CONFIG_QUOTA && \
+#./scripts/config --disable CONFIG_RFKILL && \
+#./scripts/config --disable CONFIG_SECURITY_SELINUX && \
+#./scripts/config --disable CONFIG_SOUND && \
+#./scripts/config --disable CONFIG_USB && \
+#./scripts/config --disable CONFIG_WLAN && \
+#./scripts/config --disable CONFIG_WIRELESS && \
+#./scripts/config --disable CONFIG_XFRM && \
+#./scripts/config --disable CONFIG_XFRM_OFFLOAD && \
+#./scripts/config --disable CONFIG_XFRM_STATISTICS && \
+#./scripts/config --disable CONFIG_XFRM_ALGO && \
+#./scripts/config --disable CONFIG_XFRM_USER && \
+#./scripts/config --disable CONFIG_INET_ESP && \
+#./scripts/config --disable CONFIG_INET_ESP_OFFLOAD && \
+#./scripts/config --disable CONFIG_INET_ESPINTCP && \
+#./scripts/config --disable CONFIG_INET_IPCOMP && \
+#./scripts/config --disable CONFIG_INET_XFRM_TUNNEL && \
+#./scripts/config --disable CONFIG_INET_TUNNEL && \
+#./scripts/config --disable CONFIG_INET6_ESP && \
+#./scripts/config --disable CONFIG_INET6_IPCOMP && \
+#./scripts/config --disable CONFIG_INET6_XFRM_TUNNEL && \
+#./scripts/config --disable CONFIG_INET6_TUNNEL && \
+
 make defconfig && \
-./scripts/config --disable CONFIG_ACPI_AC && \
-./scripts/config --disable CONFIG_ACPI_BATTERY && \
-./scripts/config --disable CONFIG_ACPI_BUTTON && \
-./scripts/config --disable CONFIG_ACPI_DOCK && \
-./scripts/config --disable CONFIG_ACPI_FAN && \
-./scripts/config --disable CONFIG_ACPI_THERMAL && \
-./scripts/config --disable CONFIG_ACPI_TINY_POWER_BUTTON && \
-./scripts/config --disable CONFIG_ACPI_VIDEO && \
-./scripts/config --enable CONFIG_BRIDGE && \
-./scripts/config --disable CONFIG_DRM && \
-./scripts/config --disable CONFIG_EFI && \
-./scripts/config --disable CONFIG_ETHTOOL_NETLINK && \
-./scripts/config --disable CONFIG_EXT4_FS && \
-./scripts/config --disable CONFIG_HID && \
-./scripts/config --disable CONFIG_INPUT && \
-./scripts/config --disable CONFIG_INPUT_MOUSE && \
-./scripts/config --disable CONFIG_IPV6 && \
-./scripts/config --disable CONFIG_MACINTOSH_DRIVERS && \
-./scripts/config --enable CONFIG_NETFILTER && \
-./scripts/config --disable CONFIG_NETWORK_FILESYSTEMS && \
-./scripts/config --disable CONFIG_PCMCIA && \
-./scripts/config --disable CONFIG_PM && \
-./scripts/config --disable CONFIG_QUOTA && \
-./scripts/config --disable CONFIG_RFKILL && \
-./scripts/config --disable CONFIG_SECURITY_SELINUX && \
-./scripts/config --disable CONFIG_SOUND && \
-./scripts/config --disable CONFIG_USB && \
-./scripts/config --disable CONFIG_WLAN && \
-./scripts/config --disable CONFIG_WIRELESS && \
-./scripts/config --disable CONFIG_XFRM && \
-./scripts/config --disable CONFIG_XFRM_OFFLOAD && \
-./scripts/config --disable CONFIG_XFRM_STATISTICS && \
-./scripts/config --disable CONFIG_XFRM_ALGO && \
-./scripts/config --disable CONFIG_XFRM_USER && \
-./scripts/config --disable CONFIG_INET_ESP && \
-./scripts/config --disable CONFIG_INET_ESP_OFFLOAD && \
-./scripts/config --disable CONFIG_INET_ESPINTCP && \
-./scripts/config --disable CONFIG_INET_IPCOMP && \
-./scripts/config --disable CONFIG_INET_XFRM_TUNNEL && \
-./scripts/config --disable CONFIG_INET_TUNNEL && \
-./scripts/config --disable CONFIG_INET6_ESP && \
-./scripts/config --disable CONFIG_INET6_IPCOMP && \
-./scripts/config --disable CONFIG_INET6_XFRM_TUNNEL && \
-./scripts/config --disable CONFIG_INET6_TUNNEL && \
+cat extra.config >> .config && \
 make -j8
 if [ $? -ne 0 ] ; then
 	exit 125 # build failed, skip current revision.
