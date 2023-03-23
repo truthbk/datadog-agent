489ff96fe524aadcaac2ff076bb3a085dac4aa9e
Branden Clark <branden.clark@datadoghq.com>
2023-03-23 17:12:52 -0400

    [skip ci] copy paste

--- test/kitchen/site-cookbooks/dd-agent-install/recipes/_install_windows_base.rb
+++ test/kitchen/site-cookbooks/dd-agent-install/recipes/_install_windows_base.rb
@@ -82,7 +82,14 @@ end
 #  action :run
 #end
 
-include_recipe 'dd-agent-install::_print_windows_installer_log'
+ruby_block "Print install logs" do
+  only_if { ::File.exists?(installer_log_file_name) }
+  block do
+    # Use warn, because Chef's default "log" is too chatty
+    # and the kitchen tests default to "warn"
+    Chef::Log.warn(File.open(installer_log_file_name, "rb:UTF-16LE", &:read).encode('UTF-8'))
+  end
+end
 
 ruby_block "Check install sucess" do
   not_if { agent_install_options.include?('WIXFAILWHENDEFERRED') }
