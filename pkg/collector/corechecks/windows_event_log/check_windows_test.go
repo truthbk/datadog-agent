3ffed3b55c0a62544fed39c060f6e3e3be7d7e3c
Branden Clark <branden.clark@datadoghq.com>
2023-03-28 12:06:46 -0400

    add benchmark, reorg test to avoid mock memory issues

--- pkg/collector/corechecks/windows_event_log/check_windows_test.go
+++ pkg/collector/corechecks/windows_event_log/check_windows_test.go
@@ -10,6 +10,7 @@ package evtlog
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
 	"github.com/DataDog/datadog-agent/pkg/autodiscovery/integration"
@@ -27,6 +28,7 @@ type GetEventsTestSuite struct {
 	testAPI     string
 	numEvents   uint
 
+	sender *mocksender.MockSender
 	ti eventlog_test.APITester
 }
 
@@ -36,6 +38,7 @@ func (s *GetEventsTestSuite) SetupSuite() {
 	require.NoError(s.T(), err)
 	err = s.ti.InstallSource(s.channelPath, "testsource")
 	require.NoError(s.T(), err)
+	s.sender = mocksender.NewMockSender("")
 }
 
 func (s *GetEventsTestSuite) TearDownSuite() {
@@ -47,12 +50,13 @@ func (s *GetEventsTestSuite) SetupTest() {
 	// Ensure the log is empty
 	err := s.ti.API().EvtClearLog(s.channelPath)
 	require.NoError(s.T(), err)
-
+	s.sender.ResetCalls()
 }
 
 func (s *GetEventsTestSuite) TearDownTest() {
 	err := s.ti.API().EvtClearLog(s.channelPath)
 	require.NoError(s.T(), err)
+	s.sender.ResetCalls()
 }
 
 func TestLaunchGetEventsTestSuite(t *testing.T) {
@@ -63,10 +67,26 @@ func TestLaunchGetEventsTestSuite(t *testing.T) {
 			var s GetEventsTestSuite
 			s.channelPath = "testchannel"
 			s.testAPI = tiName
-			s.numEvents = 10
+			s.numEvents = 1000
 			suite.Run(t, &s)
 		})
 	}
+}
+
+func readEvents(check *Check, senderEventCall *mock.Call, numEvents uint) uint {
+	eventsCollected := uint(0)
+	prevEventsCollected := uint(0)
+	senderEventCall.Run(func (args mock.Arguments) {
+		eventsCollected += 1
+	})
+	for {
+		check.Run()
+		if eventsCollected == numEvents || prevEventsCollected == eventsCollected{
+			break
+		}
+		prevEventsCollected = eventsCollected
+	}
+	return eventsCollected
 }
 
 func (s *GetEventsTestSuite) TestGetEvents() {
@@ -84,13 +104,83 @@ start: old
 	check.evtapi = s.ti.API()
 	err = check.Configure(integration.FakeConfigHash, instanceConfig, nil, "test")
 	require.NoError(s.T(), err)
+	defer check.Cancel()
 
-	sender := mocksender.NewMockSender(check.ID())
+	mocksender.SetSender(s.sender, check.ID())
+	s.sender.On("Commit").Return()
+	senderEventCall := s.sender.On("Event", mock.Anything)
 
-	sender.On("Commit").Return().Times(1)
-	sender.On("Event", mock.Anything).Return().Times(int(s.numEvents))
-	check.Run()
+	eventsCollected := readEvents(check, senderEventCall, s.numEvents)
 
-	sender.AssertExpectations(s.T())
-	sender.AssertNumberOfCalls(s.T(), "Commit", 1)
+	require.Equal(s.T(), s.numEvents, eventsCollected)
+	s.sender.AssertExpectations(s.T())
+}
+
+func BenchmarkGetEvents(b *testing.B) {
+	channelPath := "testchannel"
+	numEvents := []uint{10, 100, 1000}
+	batchCounts := []uint{1, 10, 100, 1000}
+
+	testerNames := eventlog_test.GetEnabledAPITesters()
+
+	sender := mocksender.NewMockSender("")
+	sender.On("Commit").Return()
+	senderEventCall := sender.On("Event", mock.Anything)
+
+	bench_startTime := time.Now()
+	bench_total_events := uint(0)
+	for _, tiName := range testerNames {
+		for _, v := range numEvents {
+			// setup log
+			ti := eventlog_test.GetAPITesterByName(tiName, b)
+			err := ti.InstallChannel(channelPath)
+			require.NoError(b, err)
+			err = ti.InstallSource(channelPath, "testsource")
+			require.NoError(b, err)
+			err = ti.API().EvtClearLog(channelPath)
+			require.NoError(b, err)
+			err = ti.GenerateEvents(channelPath, v)
+			require.NoError(b, err)
+
+			for _, batchCount := range batchCounts {
+				if batchCount > v {
+					continue
+				}
+				b.Run(fmt.Sprintf("%vAPI/%dEvents/%dBatch", tiName, v, batchCount), func(b *testing.B) {
+					// setup check
+					instanceConfig := []byte(fmt.Sprintf(`
+path: %s
+start: old
+payload_size: %d
+`,
+					channelPath, batchCount))
+
+					// read the log b.N times
+					b.ResetTimer()
+					startTime := time.Now()
+					total_events := uint(0)
+					for i:= 0; i < b.N; i++ {
+						check := new(Check)
+						check.evtapi = ti.API()
+						err = check.Configure(integration.FakeConfigHash, instanceConfig, nil, "test")
+						require.NoError(b, err)
+						mocksender.SetSender(sender, check.ID())
+
+						total_events += readEvents(check, senderEventCall, v)
+
+						check.Cancel()
+						sender.ResetCalls()
+					}
+
+					// TODO: Use b.Elapsed in go1.20
+					elapsed := time.Since(startTime)
+					b.Logf("%.2f events/s (%.3fs) N=%d", float64(total_events)/elapsed.Seconds(), elapsed.Seconds(), b.N)
+					bench_total_events += total_events
+				})
+			}
+		}
+	}
+
+	elapsed := time.Since(bench_startTime)
+	b.Logf("Benchmark total: %d events %.2f events/s (%.3fs)", bench_total_events, float64(bench_total_events)/elapsed.Seconds(), elapsed.Seconds())
 }
