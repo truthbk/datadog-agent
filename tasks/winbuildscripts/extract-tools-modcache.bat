abfc49eefb1b794604700a0442efadadf3ff60c4
Kylian Serrania <kylian.serrania@datadoghq.com>
2023-04-07 21:21:20 +0200

    Do not extract zip file in volume

--- tasks/winbuildscripts/extract-tools-modcache.bat
+++ tasks/winbuildscripts/extract-tools-modcache.bat
@@ -1,13 +1,13 @@
-if exist c:\mnt\modcache_tools.tar.gz (
+if exist c:\dev\go\src\github.com\DataDog\datadog-agent\modcache_tools.tar.gz (
     @echo Extracting modcache_tools
-    Powershell -C "7z x c:\mnt\modcache_tools.tar.gz -oc:\mnt"
+    Powershell -C "7z x c:\dev\go\src\github.com\DataDog\datadog-agent\modcache_tools.tar.gz -oc:\dev\go\src\github.com\DataDog\datadog-agent"
     REM Use -aoa to allow overwriting existing files
     REM This shouldn't have any negative impact: since modules are
     REM stored per version and hash, files that get replaced will
     REM get replaced by the same files
-    Powershell -C "7z x c:\mnt\modcache_tools.tar -oc:\modcache -aoa"
-    del /f c:\mnt\modcache_tools.tar.gz
-    del /f c:\mnt\modcache_tools.tar
+    Powershell -C "7z x c:\dev\go\src\github.com\DataDog\datadog-agent\modcache_tools.tar -oc:\modcache -aoa"
+    del /f c:\dev\go\src\github.com\DataDog\datadog-agent\modcache_tools.tar.gz
+    del /f c:\dev\go\src\github.com\DataDog\datadog-agent\modcache_tools.tar
     @echo modcache_tools extracted
 ) else (
     @echo modcache_tools.tar.gz not found, tooling dependencies will be downloaded
--- tasks/winbuildscripts/unittests.bat
+++ tasks/winbuildscripts/unittests.bat
@@ -7,12 +7,11 @@ if not exist c:\mnt\ goto nomntdir
 
 if NOT DEFINED PY_RUNTIMES set PY_RUNTIMES=%~1
 
-call %~p0extract-modcache.bat
-call %~p0extract-tools-modcache.bat
-
 mkdir \dev\go\src\github.com\DataDog\datadog-agent
 cd \dev\go\src\github.com\DataDog\datadog-agent
 xcopy /e/s/h/q c:\mnt\*.*
 
+call %~p0extract-modcache.bat
+call %~p0extract-tools-modcache.bat
 
 Powershell -C "c:\mnt\tasks\winbuildscripts\unittests.ps1"
