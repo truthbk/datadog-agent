e8aea702859316e76ca5dfc64807d6ac2aa0fa24
Brian Floersch <brian.floersch@datadoghq.com>
2023-04-27 13:16:45 -0400

    adding tests

--- pkg/logs/internal/tailers/tailer.go
+++ pkg/logs/internal/tailers/tailer.go
@@ -2,13 +2,10 @@ package tailers
 
 import (
 	"github.com/DataDog/datadog-agent/pkg/logs/internal/status"
-	"github.com/DataDog/datadog-agent/pkg/util/startstop"
 )
 
 // Tailer the base interface for a tailer.
 type Tailer interface {
-	startstop.Stoppable
-
 	GetId() string
 	GetType() string
 	GetInfo() *status.InfoRegistry
