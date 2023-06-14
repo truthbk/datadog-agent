cea8b189ef48e3153f1a306d6c7a7d2c2d1296b9
usamasaqib <usamasaqib.96@live.com>
2023-06-14 15:35:53 +0200

    az2 try3

--- test/new-e2e/system-probe/system-probe-test-env.go
+++ test/new-e2e/system-probe/system-probe-test-env.go
@@ -35,7 +35,7 @@ var (
 
 const az1 = "subnet-03061a1647c63c3c3,subnet-071213aedb0e1ae54,subnet-0f1ca3e929eb3fb8b"
 
-// try 2
+// try 3
 const az2 = "subnet-071213aedb0e1ae54,subnet-0f1ca3e929eb3fb8b,subnet-03061a1647c63c3c3"
 const az3 = "subnet-0f1ca3e929eb3fb8b,subnet-03061a1647c63c3c3,subnet-071213aedb0e1ae54"
 
