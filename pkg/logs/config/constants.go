d0be56bc83c70b68f3a00b7da99b31e3949e6e14
Maxime David <maxime.david@datadoghq.com>
2023-06-16 08:40:36 +0200

    add comment

--- pkg/logs/config/constants.go
+++ pkg/logs/config/constants.go
@@ -9,7 +9,7 @@ package config
 const (
 	ChanSize                   = 100
 	DestinationPayloadChanSize = 10
-	NumberOfPipelines          = 1
+	NumberOfPipelines          = 1 // not sure this change is needed
 )
 
 const (
