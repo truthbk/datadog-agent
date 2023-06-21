6cfbe94a22183bb417d699e9976cff57cbebe1a8
Branden Clark <branden.clark@datadoghq.com>
2023-06-20 20:51:06 -0400

    modcache test1

--- tasks/winbuildscripts/extract-modcache.bat
+++ tasks/winbuildscripts/extract-modcache.bat
@@ -1,7 +1,9 @@
 if exist c:\mnt\modcache.tar.gz (
     @echo Extracting modcache
+    Powershell -C "get-date"
     Powershell -C "7z x c:\mnt\modcache.tar.gz -oc:\mnt"
     Powershell -C "7z x c:\mnt\modcache.tar -oc:\modcache"
+    Powershell -C "get-date"
     del /f c:\mnt\modcache.tar.gz
     del /f c:\mnt\modcache.tar
     @echo Modcache extracted
