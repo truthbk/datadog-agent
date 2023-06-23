7e94ab1d2131bcffc7cc53b39d39cf5491e094a8
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2023-06-22 22:15:15 -0700

    Add DriverServiceStatus function

--- test/new-e2e/utils/e2e/client/apmautoinjector.go
+++ test/new-e2e/utils/e2e/client/apmautoinjector.go
@@ -6,6 +6,14 @@ import (
 	"github.com/DataDog/test-infra-definitions/components/datadog/apmautoinjector"
 )
 
+type DriverStatus string
+
+const (
+	NotInstalled DriverStatus = "NOTINSTALLED"
+	Running      DriverStatus = "RUNNING"
+	Stopped      DriverStatus = "STOPPED"
+)
+
 var _ clientService[apmautoinjector.ClientData] = (*APMAutoInjector)(nil)
 
 // A client APMAutoInjector that is connected to an [apmautoinjector.Installer].
@@ -19,6 +27,11 @@ func NewAPMAutoInjector(installer *apmautoinjector.Installer) *APMAutoInjector {
 	injectorInstance := &APMAutoInjector{}
 	injectorInstance.UpResultDeserializer = NewUpResultDeserializer[apmautoinjector.ClientData](installer, injectorInstance)
 	return injectorInstance
+}
+
+func (inj *APMAutoInjector) DriverServiceStatus() (DriverStatus, error) {
+	output, err := inj.vmClient.ExecuteWithError("try { (get-service \"ddapm\" -ErrorAction Stop).Status } catch { write-host \"NOTINSTALLED\" }\"")
+	return DriverStatus(output), err
 }
 
 //lint:ignore U1000 Ignore unused function as this function is call using reflection
