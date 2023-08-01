080638a39bc63eee8d0c94009511f47c337dfc38
DanielLavie <daniel.lavie@datadoghq.com>
2023-08-01 16:36:08 +0300

    WIP

--- pkg/network/usm/events/usm_consumer.go
+++ pkg/network/usm/events/usm_consumer.go
@@ -9,16 +9,31 @@ package events
 
 import (
 	"github.com/DataDog/datadog-agent/pkg/eventmonitor"
+	"github.com/DataDog/datadog-agent/pkg/runtime"
 	smodel "github.com/DataDog/datadog-agent/pkg/security/secl/model"
 )
 
 type USMConsumer struct{}
 
 func (n *USMConsumer) Start() error {
-	return nil
+	cpuNum := runtime.NumVCPU()
+	callbackRunner := make(chan func(), pendingCallbacksQueueSize)
+	pm.callbackRunnersWG.Add(cpm)
+	for i := 0; i < cpuNum; i++ {
+		go func() {
+			defer pm.callbackRunnersWG.Done()
+			for call := range pm.callbackRunner {
+				if call != nil {
+					call()
+				}
+			}
+		}()
+	}
 }
 
 func (n *USMConsumer) Stop() {
+	// TODO:  Wait for all callback runners to stop. We need to be careful here not create a deadlock as this function
+	// is called in the context of the event monitor module, so the callbackRunner channel is closed before waiting on anything
 }
 
 // ID returns id for process monitor
@@ -28,11 +43,11 @@ func (n *USMConsumer) ID() string {
 
 // NewUSMConsumer returns a new USMConsumer instance
 func NewUSMConsumer(evm *eventmonitor.EventMonitor) (*USMConsumer, error) {
-	h := &eventHandler{}
-	if err := evm.AddEventTypeHandler(smodel.ExecEventType, h); err != nil {
+	eventHandler := newEventHandler()
+	if err := evm.AddEventTypeHandler(smodel.ExecEventType, eventHandler); err != nil {
 		return nil, err
 	}
-	if err := evm.AddEventTypeHandler(smodel.ExitEventType, h); err != nil {
+	if err := evm.AddEventTypeHandler(smodel.ExitEventType, eventHandler); err != nil {
 		return nil, err
 	}
 
--- pkg/network/usm/events/usm_handler.go
+++ pkg/network/usm/events/usm_handler.go
@@ -1,14 +1,99 @@
 package events
 
 import (
+	"github.com/DataDog/datadog-agent/pkg/runtime"
+	"sync"
+
+	"go.uber.org/atomic"
+
 	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
+	smodel "github.com/DataDog/datadog-agent/pkg/security/secl/model"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
-type eventHandler struct{}
+const (
+	// The size of the callbacks queue for pending tasks.
+	pendingCallbacksQueueSize = 1000
+)
 
-func (h *eventHandler) HandleEvent(ev *model.Event) {
+type eventHandler struct {
+	sync.RWMutex
+
+	hasExecCallbacks          atomic.Bool
+	processExecCallbacksMutex sync.RWMutex
+	processExecCallbacks      map[*ProcessCallback]struct{}
+
+	hasExitCallbacks          atomic.Bool
+	processExitCallbacksMutex sync.RWMutex
+	processExitCallbacks      map[*ProcessCallback]struct{}
+
+	callbackRunner chan func()
+
+	execCount atomic.Uint32
+}
+
+type ProcessCallback func(pid int)
+
+func newEventHandler() *eventHandler {
+	cpuNum := runtime.NumVCPU()
+	callbackRunner := make(chan func(), pendingCallbacksQueueSize)
+	pm.callbackRunnersWG.Add(cpm)
+	for i := 0; i < cpuNum; i++ {
+		go func() {
+			defer pm.callbackRunnersWG.Done()
+			for call := range pm.callbackRunner {
+				if call != nil {
+					call()
+				}
+			}
+		}()
+	}
+}
+
+func (eh *eventHandler) HandleEvent(ev *model.Event) {
 	ev.ResolveFields()
 
 	log.Debugf("usm handler %w", ev.ProcessContext)
+	switch ev.GetEventType() {
+	case smodel.ExecEventType:
+		log.Debug("Handling exec event")
+		// handleProcessExec locks a mutex to access the exec callbacks array, if it is empty, then we're
+		// wasting "resources" to check it. Since it is a hot-code-path, it has some cpu load.
+		// Checking an atomic boolean, is an atomic operation, hence much faster.
+		if eh.hasExecCallbacks.Load() {
+			eh.handleProcessExec(int(ev.PIDContext.Pid))
+		}
+	case smodel.ExitEventType:
+		log.Debug("Handling exit event")
+		// handleProcessExit locks a mutex to access the exit callbacks array, if it is empty, then we're
+		// wasting "resources" to check it. Since it is a hot-code-path, it has some cpu load.
+		// Checking an atomic boolean, is an atomic operation, hence much faster.
+		if eh.hasExitCallbacks.Load() {
+			eh.handleProcessExit(int(ev.PIDContext.Pid))
+		}
+	}
+}
+
+// handleProcessExec is a callback function called on a given pid that represents a new process.
+// we're iterating the relevant callbacks and trigger them.
+func (eh *eventHandler) handleProcessExec(pid int) {
+	eh.processExecCallbacksMutex.RLock()
+	defer eh.processExecCallbacksMutex.RUnlock()
+
+	for callback := range eh.processExecCallbacks {
+		temporaryCallback := callback
+		eh.callbackRunner <- func() { (*temporaryCallback)(pid) }
+	}
+}
+
+// handleProcessExit is a callback function called on a given pid that represents an exit event.
+// we're iterating the relevant callbacks and trigger them.
+func (eh *eventHandler) handleProcessExit(pid int) {
+	eh.processExitCallbacksMutex.RLock()
+	defer eh.processExitCallbacksMutex.RUnlock()
+
+	for callback := range eh.processExitCallbacks {
+		temporaryCallback := callback
+		eh.callbackRunner <- func() { (*temporaryCallback)(pid) }
+	}
 }
