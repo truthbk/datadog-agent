d3b293d79f02abdcf7fc5fc2fbe9d0784cb5f227
Branden Clark <branden.clark@datadoghq.com>
2023-08-30 19:27:49 -0400

    append instead of overwrite

--- cmd/agent/debuglog.h
+++ cmd/agent/debuglog.h
@@ -1,4 +1,7 @@
 #include <Windows.h>
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
 
 __attribute__((constructor)) void dostuff() {
     if (GetStdHandle(STD_OUTPUT_HANDLE) != NULL || GetStdHandle(STD_ERROR_HANDLE) != NULL)
@@ -7,9 +10,45 @@ __attribute__((constructor)) void dostuff() {
         return;
     }
 
-    HANDLE hout = CreateFileW(L"C:\\ProgramData\\Datadog\\logs\\agent-stdout.log", GENERIC_WRITE, FILE_READ_ACCESS | FILE_WRITE_ACCESS, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    HANDLE herr = CreateFileW(L"C:\\ProgramData\\Datadog\\logs\\agent-stderr.log", GENERIC_WRITE, FILE_READ_ACCESS | FILE_WRITE_ACCESS, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    // open files
+    HANDLE hout = CreateFileW(L"C:\\ProgramData\\Datadog\\logs\\agent-stdout.log", FILE_APPEND_DATA, FILE_READ_ACCESS | FILE_WRITE_ACCESS, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE herr = CreateFileW(L"C:\\ProgramData\\Datadog\\logs\\agent-stderr.log", FILE_APPEND_DATA, FILE_READ_ACCESS | FILE_WRITE_ACCESS, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
-    SetStdHandle(STD_OUTPUT_HANDLE, hout);
-    SetStdHandle(STD_ERROR_HANDLE, herr);
+    // set handles picked up by golang
+    if (INVALID_HANDLE_VALUE != hout)
+    {
+        SetStdHandle(STD_OUTPUT_HANDLE, hout);
+    }
+    if (INVALID_HANDLE_VALUE != herr)
+    {
+        SetStdHandle(STD_ERROR_HANDLE, herr);
+    }
+
+    // log timestamp to differentiate runs
+    if (INVALID_HANDLE_VALUE != herr)
+    {
+        int fd_err = _open_osfhandle((intptr_t)herr, _O_APPEND);
+        if (-1 == fd_err)
+        {
+            goto msvcrt_err_end;
+        }
+        FILE *ferr = fdopen(fd_err, "a");
+        if (NULL == ferr)
+        {
+            goto msvcrt_err_end;
+        }
+        SYSTEMTIME t;
+        GetSystemTime(&t);
+        fprintf(ferr, "%d-%02d-%02d %02d:%02d:%02d.%03d: agent starting\n",
+            t.wYear,
+            t.wMonth,
+            t.wDay,
+            t.wHour,
+            t.wMinute,
+            t.wSecond,
+            t.wMilliseconds);
+        fflush(ferr);
+msvcrt_err_end: {}
+    }
+
 }
