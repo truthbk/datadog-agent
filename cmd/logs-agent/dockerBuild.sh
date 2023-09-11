d8d7da6cbc787b91f4ad4e5e9c5b4b64b846dc73
Brian Floersch <brian.floersch@datadoghq.com>
2023-09-11 16:35:37 +0200

    buildx

--- cmd/logs-agent/dockerBuild.sh
+++ cmd/logs-agent/dockerBuild.sh
@@ -2,4 +2,10 @@
 
 cd ../..
 
-docker build --progress plain -f cmd/logs-agent/Dockerfile -t logs-image .
+docker buildx build \
+--platform linux/amd64,linux/arm64 \
+-t bfloerschddog/logs-only-agent:latest \
+-f cmd/logs-agent/Dockerfile \
+--push .
+
+# docker build --progress plain -f cmd/logs-agent/Dockerfile -t logs-image .
