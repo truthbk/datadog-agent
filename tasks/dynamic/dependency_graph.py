1ba0ad9e1f5813b0c43ad389ed40160fbe77aee9
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-09-13 13:58:16 +0200

    test(pipeline): test pipeline generation

---
+++ tasks/dynamic/dependency_graph.py
@@ -0,0 +1,207 @@
+import oyaml as yaml
+
+
+class DepNode:
+    """
+    Base dependency graph node
+    :name Job name
+    :data Associated to the node usually boolean
+    :children List of the node's children
+    """
+    def __init__(self, name, data, children, parent=None):
+        if parent is None:
+            parent = []
+        self.name = name
+        self.data = data
+        self.children = children
+        self.parent = parent
+
+
+class DepGraph:
+    def __init__(self):
+        self.stage_list = []
+        self.job_tags = {}
+        self.init_graph()
+        self.root = DepNode("StartNode", False, [])
+        self.ghost_edges = {}
+        self.job_lists = []
+        self.seen_map = []
+        self.debug = False
+
+    def init_graph(self):
+        """
+        Setuo stage node of the dependency graph
+        :return: (DepNode) The root of the graph
+        """
+        with open(".dynamic.yml", "r") as f:
+            yaml_content = yaml.load(f.read(), Loader=yaml.SafeLoader)
+        self.stage_list = list(yaml_content["stages"])
+        self.job_tags = {stage: [] for stage in self.stage_list}
+        self.job_tags["no-stage"] = []
+        self.job_tags["no-dep"] = []
+
+    def find_node(self, node_name): # TODO: Slow because of seen map
+        if not self.root:
+            return None
+
+        queue = [self.root]
+        while queue:
+            node = queue.pop(0)
+            self.seen_map.append(node.name)
+            if node.name == node_name:
+                return node
+            for child in node.children:
+                if child.name not in self.seen_map:
+                    queue.append(child)
+        return None
+
+    def propagate_node_state(self, node_name, new_state, reverse=False): # TODO: Slow because of seen map
+        self.seen_map = []
+        node = self.find_node(node_name)
+        self.seen_map = []
+        if node is not None:
+            if not reverse:
+                self.__propagate_node_state(node, new_state, 0)
+            else:
+                self.__propagate_parent_node_state(node,new_state, 0)
+
+    def __propagate_node_state(self, node, new_state, distance):
+        if node.name in self.seen_map:
+            return
+        if distance > 4:
+            distance = 4
+        if new_state:
+            node.data = "activated_" + str(int(distance))
+        else:
+            node.data = new_state
+        self.seen_map.append(node.name)
+        for child in node.children:
+            self.__propagate_node_state(child, new_state, distance + 0.5)
+
+    def __propagate_parent_node_state(self, node, new_state, distance):
+        if node.name in self.seen_map:
+            return
+        if distance > 4:
+            distance = 4
+        if new_state:
+            node.data = "activated_" + str(int(distance))
+        else:
+            node.data = new_state
+        self.seen_map.append(node.name)
+        if not node.parent:
+            return
+        for child in node.parent:
+            self.__propagate_parent_node_state(child, new_state, distance + 0.5)
+
+    def __insert_node_after_dep(self, dep_nodes, new_node):
+        for node in dep_nodes:
+            self.__append_node_children(node, new_node)
+
+
+
+    def __append_node_children(self, node, new_node):
+        if new_node not in node.children:
+            node.children.append(new_node)
+        if node not in new_node.parent:
+            new_node.parent.append(node)
+
+    def __parse_dependencies(self, deps):
+        if deps is None:
+            return None
+        for k,elem in enumerate(deps):
+            if type(elem) == dict:
+                if elem.get('job', None) is not None:
+                    deps[k] = elem['job']
+        return deps
+
+    def append_node(self, job_name, stage, dependencies=[]):
+        dependencies = self.__parse_dependencies(dependencies)
+
+        if job_name[0] == ".":
+            return
+        if self.debug:
+            print(f"Call to append_node with {job_name} {stage} {dependencies}")
+            self.job_lists.append(job_name)
+        new_node = DepNode(job_name, False, [])
+        self.job_tags[stage].append(new_node)
+        queue = [self.root, None]
+        current_found_deps = []
+        depth = 1
+        while queue and dependencies:
+            node = queue.pop(0)
+            if node is not None:
+                if node.name in dependencies:
+                    current_found_deps.append(node)
+                    dependencies.remove(node.name)
+                if not dependencies:
+                    self.__insert_node_after_dep(current_found_deps, new_node)
+                    #self.__ghost_edge_resolve(new_node)
+                    return
+                else:
+                    for child in node.children:
+                        if child not in queue:
+                            queue.append(child)
+            else:
+                depth += 1
+                if queue:
+                    queue.append(None)
+                else:  # missing dependencies -> create ghost edge
+                    for missing_dep in dependencies:
+                        if self.ghost_edges.get(missing_dep, None) is None:
+                            self.ghost_edges[missing_dep] = [new_node]
+                        else:
+                            self.ghost_edges[missing_dep].append(new_node)
+                    break
+
+        if dependencies is None:
+            self.__insert_node_after_dep([self.root], new_node)
+        elif not dependencies:
+            self.job_tags["no-dep"].append((new_node, stage))
+
+        #self.__ghost_edge_resolve(new_node)
+
+    def __ghost_edge_resolve(self, new_node):
+        if self.ghost_edges.get(new_node.name, None) is not None:
+            for dep in self.ghost_edges[new_node.name]:
+                self.__append_node_children(new_node, dep)
+                self.__ghost_edge_resolve(dep)
+            del self.ghost_edges[new_node.name]
+
+    def resolve_stage_dep(self):
+        #print([(n.name, s) for n, s in self.job_tags["no-dep"]])
+        for new_node, stage in self.job_tags["no-dep"]:
+            if stage == "no-stage":
+                continue
+            try:
+                stage_index = self.stage_list.index(stage)
+            except ValueError:
+                print("Wrong stage name : ", stage)
+                return
+            if stage_index > 0:
+                self.__insert_node_after_dep(self.job_tags[self.stage_list[stage_index - 1]], new_node)
+            else:
+                self.__append_node_children(self.root, new_node)
+        del self.job_tags["no-dep"]
+
+        for stage in self.job_tags:
+            for node in self.job_tags[stage]:
+                self.__ghost_edge_resolve(node)
+        if self.debug:
+            print("Unresolved ghost dependency : ", sum([len(elem[1]) for elem in self.ghost_edges.items()]))
+            print(self.ghost_edges.keys())
+            print(len(self.job_lists))
+
+    def pipeline_jobs_to_run(self, needed_jobs_list):
+        self.propagate_node_state("StartNode", False)
+        for job in needed_jobs_list:
+            self.propagate_node_state(job, True, True)
+        queue = []
+        nodes = []
+        while queue:
+            node = queue.pop(0)
+            if node.data:
+                nodes.append(node.name)
+            for child in node.children:
+                queue.append(child)
+        return nodes
+
---
+++ tasks/dynamic/parsing.py
@@ -0,0 +1,289 @@
+from dataclasses import dataclass
+
+import oyaml as yaml
+import glob
+from . import dependency_graph
+import re
+import os
+
+"""
+Yaml Loader edit to allow '!reference' tags support
+"""
+
+@dataclass
+class reference:
+    content: str
+
+class Loader(yaml.SafeLoader):
+    pass
+
+class Dumper(yaml.Dumper):
+    def process_scalar(self):
+        if self.analysis is None:
+            self.analysis = self.analyze_scalar(self.event.value)
+        if self.style is None:
+            self.style = self.choose_scalar_style()
+        split = (not self.simple_key_context)
+        #if self.analysis.multiline and split    \
+        #        and (not self.style or self.style in '\'\"'):
+        #    self.write_indent()
+        if self.analysis.scalar.startswith("!reference"):
+            self.style = ""
+
+        if self.style == '"':
+            self.write_double_quoted(self.analysis.scalar, split)
+        elif self.style == '\'':
+            self.write_single_quoted(self.analysis.scalar, split)
+        elif self.style == '>':
+            self.write_folded(self.analysis.scalar)
+        elif self.style == '|':
+            self.write_literal(self.analysis.scalar)
+        else:
+            self.write_plain(self.analysis.scalar, split)
+        self.analysis = None
+        self.style = None
+
+
+
+class AsHex:
+    def __init__(self,data):
+        self.data = data
+
+    def __repr__(self):
+        return self.data
+
+def represent_hex(dumper, data):
+    #return yaml.Dumper.represent_tuple(dumper, data.data)
+    #return yaml.ScalarNode("tag:yaml.org,2002:int", data, style="")
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data.data, style='')
+
+
+yaml.add_representer(AsHex, represent_hex)
+
+
+def construct_reference(loader, node):
+    content = str(node.tag+"["+",".join([e.value for e in node.value])+"]")
+    return AsHex(content)
+    # return reference(node.tag+"["+",".join([e.value for e in node.value])+"]")
+
+
+Loader.add_constructor("!reference", construct_reference)
+
+def createAndOpen(filename, mode):
+     os.makedirs(os.path.dirname(filename), exist_ok=True)
+     return open(filename, mode)
+
+class InvalidGitlabYamlExtension(Exception):
+    """
+    Custom Yaml Parsing Exception
+    """
+
+    def __init__(self, yaml_content, message="Invalid Gitlab Extension !"):
+        self.yaml_content = yaml_content
+        self.message = message + "\nYaml Content :\n" + str(self.yaml_content)
+        super().__init__(self.message)
+
+
+class Condition:
+    """
+    :name Condition's name
+    :cmds Commands that define the condition value
+    :condition_value Result value of the condition
+    """
+
+    def __init__(self, name, cmds):
+        self.name = name
+        self.cmds = cmds
+        self.condition_value = None
+
+    def __str__(self):
+        return f"{self.name}: {self.condition_value}"
+
+    def execute(self, ctx):
+        if not self.cmds:
+            return
+        for cmd in self.cmds:
+            result = ctx.run(cmd, hide=True)
+        self.condition_value = result.return_code == 0
+
+
+class GitlabExtender:
+    def __init__(self, invoke_ctx, source_folder=".gitlab", source_ci_file=".gitlab-ci.yml", output_folder="dynamic", debug_log=False):
+        self.deps_graph =  dependency_graph.DepGraph()
+        self.gitlab_folder = source_folder
+        self.gitlab_ci_file = source_ci_file
+        self.ctx = invoke_ctx
+        self.conditions = {}
+        self.function_registry = {}
+        self.extend_buffer = {}
+        self.extend_save = []
+        self.dep_root = None
+        self.output_folder = output_folder
+        self.debug_log = debug_log
+        isExist = os.path.exists(self.output_folder)
+        if not isExist:
+            os.makedirs(self.output_folder)
+
+    def list_jobs_dependencies(self):
+        print("Ghost dependency ignored : ", len(self.deps_graph.ghost_edges))
+        self.dep_root = self.deps_graph.root
+        data = {}
+        queue = [(self.dep_root, None)]
+        while queue:
+            node, parent = queue.pop(0)
+            node.data = True
+            if parent is not None:
+                if data.get(node.name, None) is None:
+                    data[node.name] = [parent.name]
+                elif parent.name not in data[node.name]:
+                    data[node.name].append(parent.name)
+            for child in node.children:
+                if not child.data:
+                    queue.append((child, node))
+        return data
+
+    def condition_parse(self, condition_content):
+        for condition_name in condition_content:
+            new_condition = Condition(condition_name, condition_content[condition_name])
+            try:
+                new_condition.execute(self.ctx)
+            except Exception as e:
+                new_condition.condition_value = False
+            self.conditions[condition_name] = new_condition
+
+    def if_parse(self, if_content):
+        if not (2 <= len(if_content) <= 3):
+            raise InvalidGitlabYamlExtension(if_content,
+                                             f"Gitlab(If): invalid nested element number expected 2 or 3 got {len(if_content)}")
+
+        condition_name = if_content[0]
+        true_statement = if_content[1]
+        false_statement = if_content[2]
+
+        condition_obj = self.conditions.get(condition_name, None)
+        if condition_obj is None:
+            raise InvalidGitlabYamlExtension(if_content, f"Gitlab(If): Condition {condition_name} not found !")
+
+        if condition_obj.condition_value:
+            return true_statement
+
+        return false_statement
+
+    def extended_content(self, yaml_content):
+        ext_functions = yaml_content["extends"]
+        if type(ext_functions) != list:
+            ext_functions = [ext_functions]
+        for ext_func in ext_functions:
+            if ext_func not in self.function_registry.keys():
+                print("[WARN] Function Registry not found : ", ext_func)
+                return yaml_content
+            else:
+                extension = self.function_registry[ext_func]
+                if extension.get("extends", None) is not None:
+                    extension = self.extended_content(extension)
+                extension_copy = extension.copy()
+                extension_copy.update(yaml_content)
+                return extension_copy
+
+    def quick_function_explorer(self, yaml_content, parentKey=None):
+        if type(yaml_content) == list:
+            for content in yaml_content:
+                self.quick_function_explorer(content)
+        elif type(yaml_content) == dict:
+            if yaml_content.get("extends", None) is not None:
+                value = yaml_content["extends"]
+                if type(value) != list:
+                    value = [value]
+                for ext in value:
+                    if ext[0] != ".":
+                        self.extend_save.append(ext)
+
+            if parentKey is not None and parentKey[0] == ".":
+                self.function_registry[parentKey] = yaml_content
+            elif yaml_content.get("stage", None) is not None or yaml_content.get("extends", None) is not None:
+                self.extend_buffer[parentKey] = yaml_content
+            for key in yaml_content:
+                self.quick_function_explorer(yaml_content[key], key)
+
+    def yaml_explorer(self, yaml_content, parentKey=None):
+        if type(yaml_content) == list:
+            for content in yaml_content:
+                self.yaml_explorer(content)
+        elif type(yaml_content) == dict:
+            if yaml_content.get("ext-if", None) is not None:
+                result = self.if_parse(yaml_content["ext-if"])
+                key = list(result.keys())[0]
+                yaml_content[key] = result[key]
+                yaml_content.pop("ext-if")
+            if parentKey is not None and parentKey[0] == ".":
+                pass
+            elif yaml_content.get("extends", None) is not None:
+                new_content = self.extended_content(yaml_content)
+                deps = []
+                if new_content.get("needs", None) is not None:
+                    deps = new_content["needs"]
+                if new_content.get("stage", None) is not None:
+                    self.deps_graph.append_node(parentKey, new_content["stage"], deps)
+                else:
+                    if self.debug_log:
+                        print("stage not found on ", parentKey)
+                    self.deps_graph.append_node(parentKey, "no-stage", deps)
+            elif yaml_content.get("stage", None) is not None:
+                deps = []
+                if yaml_content.get("needs", None) is not None:
+                    deps = yaml_content["needs"]
+                    if not deps:
+                        deps = None
+                self.deps_graph.append_node(parentKey, yaml_content["stage"], deps)
+            for key in yaml_content:
+                self.yaml_explorer(yaml_content[key], key)
+
+    def parse_file(self, file_path, gen_dep=False):
+        if self.debug_log:
+            print(f"Parsing {file_path}...")
+        with open(file_path, "r") as f:
+            yaml_content = yaml.load(f.read(), Loader=Loader)
+        condition_content = yaml_content.get("conditions", None)
+        if condition_content is not None:
+            self.condition_parse(condition_content)
+            yaml_content.pop("conditions")
+
+        self.yaml_explorer(yaml_content)
+        output_name = file_path.split("/")
+        if self.gitlab_folder in output_name:
+            output_name.remove(self.gitlab_folder)
+
+        with createAndOpen(f"{self.output_folder}/" + "/".join(output_name), "w") as f:
+            yaml.dump(yaml_content, f, Dumper=Dumper)
+
+    def quick_function_first_read(self, file_path):
+        with open(file_path, "r") as f:
+            yaml_content = yaml.load(f.read(), Loader=Loader)
+        self.quick_function_explorer(yaml_content)
+
+        for to_extend in self.extend_save:
+            self.function_registry[to_extend] = self.extend_buffer[to_extend]
+
+        self.extend_buffer.clear()
+        self.extend_save.clear()
+
+    def run(self):
+        self.quick_function_first_read(self.gitlab_ci_file)
+        for file in glob.glob(self.gitlab_folder + "/**/*.yml", recursive=True):
+            self.quick_function_first_read(file)
+
+        self.parse_file(self.gitlab_ci_file)
+        for file in glob.glob(self.gitlab_folder + "/**/*.yml", recursive=True):
+            self.parse_file(file)
+
+
+if __name__ == "__main__":
+    extender = GitlabExtender(None)
+    extender.run()
+    extender.deps_graph.resolve_stage_dep()
+    deps = extender.list_jobs_dependencies()
+    with open("out.csv", "w") as f:
+        content = "JobName,Dependencies"
+        for key in deps:
+            content += f"\n{key}," + ",".join(deps[key])
+        f.write(content)
