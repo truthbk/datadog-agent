fe59a6ed3ddd7985a73c92398e335ff0b40e7807
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-09-13 14:19:06 +0200

    test(pipeline): test pipeline generation

--- tasks/dynamic/commands.py
+++ tasks/dynamic/commands.py
@@ -3,6 +3,6 @@ from invoke import task
 
 @task
 def dynamic_run(ctx):
-    extender = parsing.GitlabExtender(ctx, source_ci_file="dynamic.yml")
+    extender = parsing.GitlabExtender(ctx, source_ci_file=".dynamic.yml")
     extender.run()
     extender.deps_graph.resolve_stage_dep()
